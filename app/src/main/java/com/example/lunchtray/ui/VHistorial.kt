f81271ad4c8a2843d1f363f87256be62853db406
edynson muñoz jimenez <edinsonmjim@unicauca.edu.co>
2023-06-10 18:38:24 -0500

    creacion de la navegacion, primera pantalla maquetada

---
+++ app/src/main/java/com/example/lunchtray/ui/VHistorial.kt
@@ -0,0 +1,11 @@
+package com.example.lunchtray.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun HistorialScreen(){
+    Text(text = "estas en historial de pago")
+}
---
+++ app/src/main/java/com/example/lunchtray/ui/VRegistro.kt
@@ -0,0 +1,11 @@
+package com.example.lunchtray.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun RegistroScreen(){
+    Text(text = "hola estas en registro")
+}
